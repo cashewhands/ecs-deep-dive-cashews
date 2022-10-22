20b2d857b2e4645d235b7c28a65ae6451486abaf
Demola <demoladev.ops@gmail.com>
2022-10-22 21:49:48 +0100

    created the ecs structure

--- infra/variables.tf
+++ infra/variables.tf
@@ -3,6 +3,12 @@ variable "environment" {
   description = "Environment the deployment is targetting."
 }
 
+variable "app_count" {
+  type = number
+  default = 1
+  description = "Number of resource created."
+}
+
 variable "default_tags" {
   type        = map(string)
   description = "Default tags to apply to all resources"
