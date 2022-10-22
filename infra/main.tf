456f4b0c47f7798e9cc0cb5d0959410ba0c34e5f
Demola <demoladev.ops@gmail.com>
2022-10-22 23:03:48 +0100

    checkov problem in place

--- infra/main.tf
+++ infra/main.tf
@@ -3,11 +3,11 @@ resource "aws_vpc" "default" {
 }
 
 resource "aws_flow_log" "default" {
-  iam_role_arn    = aws_iam_role.example.arn
-  log_destination = aws_s3_bucket.default.arn
+  iam_role_arn         = aws_iam_role.example.arn
+  log_destination      = aws_s3_bucket.default.arn
   log_destination_type = "s3"
-  traffic_type    = "ALL"
-  vpc_id          = aws_vpc.default.id
+  traffic_type         = "ALL"
+  vpc_id               = aws_vpc.default.id
 }
 
 resource "aws_s3_bucket" "default" {
