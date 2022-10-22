a8d405ea0409636e82b17ca1bad0de5b5bcb07b1
Demola <demoladev.ops@gmail.com>
2022-10-22 19:44:50 +0100

    new build carried out

--- infra/variables.tf
+++ infra/variables.tf
@@ -2,3 +2,8 @@ variable "environment" {
   type        = string
   description = "Environment the deployment is targetting."
 }
+
+variable "default_tags" {
+  type        = map(string)
+  description = "Default tags to apply to all resources"
+}
