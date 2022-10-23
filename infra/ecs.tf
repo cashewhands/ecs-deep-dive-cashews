c7a06af4870ecf86f49fab2f10e35d6e488fce32
Demola <demoladev.ops@gmail.com>
2022-10-24 00:16:00 +0100

    checkov error fix

--- infra/ecs.tf
+++ infra/ecs.tf
@@ -45,12 +45,16 @@ resource "aws_ecs_task_definition" "ecs-task" {
   memory                   = "512"
   cpu                      = "256"
   execution_role_arn       = aws_iam_role.ecsTaskExecutionRole.arn
-  task_role_arn            = aws_iam_role.ecsTaskExecutionRole.arn
+  /* task_role_arn            = aws_iam_role.ecsTaskExecutionRole.arn */
   depends_on               = [aws_cloudwatch_log_group.log-group]
 }
 
 resource "aws_cloudwatch_log_group" "log-group" {
+  #checkov:skip=CKV_AWS_158: Ensure that CloudWatch Log Group is encrypted by KMS
+  
   name = "${var.app_name}-${var.environment}-logs"
+
+  retention_in_days = 90
 }
 
 data "aws_ecs_task_definition" "main" {
