66e81a9e0c028a2736492a1ad5bec4624a1302f6
Demola <demoladev.ops@gmail.com>
2022-10-23 23:20:44 +0100

    checkov error fix

--- infra/ecs.tf
+++ infra/ecs.tf
@@ -46,9 +46,12 @@ resource "aws_ecs_task_definition" "ecs-task" {
   cpu                      = "256"
   execution_role_arn       = aws_iam_role.ecsTaskExecutionRole.arn
   task_role_arn            = aws_iam_role.ecsTaskExecutionRole.arn
-  depends_on = [aws_cloudwatch_log_group.log-group]
+  
 }
 
+resource "aws_cloudwatch_log_group" "log-group" {
+  name = "${var.app_name}-${var.environment}-logs"
+}
 
 data "aws_ecs_task_definition" "main" {
   task_definition = aws_ecs_task_definition.ecs-task.family
