27d3e38286de335c715a50585edcc3d9593a9cec
Demola <demoladev.ops@gmail.com>
2022-10-23 22:31:43 +0100

    checkov error fix

--- infra/lb.tf
+++ infra/lb.tf
@@ -47,7 +47,7 @@ resource "aws_lb_listener" "listener" {
   }
 }
 
-resource "aws_lb_listener" "listener-https" {
+/* resource "aws_lb_listener" "listener-https" {
   load_balancer_arn = aws_lb.application_load_balancer.id
   port              = "443"
   protocol          = "HTTPS"
@@ -59,4 +59,4 @@ resource "aws_lb_listener" "listener-https" {
     target_group_arn = aws_lb_target_group.target_group.id
     type             = "forward"
   }
-}
+} */
