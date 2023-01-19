ddd34fff685a118094aceadb022b2a247df79ddd
Demola <demoladev.ops@gmail.com>
2023-01-19 10:17:48 +0000

    Fix for the InvalidParameterValue error

--- infra/local.tf
+++ infra/local.tf
@@ -3,6 +3,6 @@ locals {
   default_tags = merge(var.default_tags, {
     Environment = var.environment
   })
-  public_subnets_ids = [for k, v in data.aws_subnets.filtered_public : v.ids[0]]
+  public_subnets_ids = [for k, v in data.aws_subnets.filtered_public : v.ids]
   test               = "this is the output"
 }
